16ec93013b34a83e8a8c054bb97441c826be5fa4
joyadeep <joyadeep.limbu@gmail.com>
2022-05-09 23:18:48 +0545

    added image border

--- src/atoms/Image.tsx
+++ src/atoms/Image.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
+// type ImageRadius={
+//   radius:"full"|"none"|"md"
+// }
 interface Iimage{
   source:string;
   alt:string;
+  radius:"full"|"none"|"md";
   style?:string;
 }
 
-const Image:React.FC<Iimage> = ({source,alt,style}) => {
+const Image:React.FC<Iimage> = ({source,alt,style,radius}) => {
   return (
-    <img src={source} alt={alt} className={` ${style} w-20 h-20 border-2 border-blue-600 rounded-full sm:rounded-sm fit-cover`} />
+    <img src={source} alt={alt} className={` ${style} w-20 h-20 border-2 border-blue-600 rounded-${radius} fit-cover`} />
   )
 }
 
--- src/molecules/User.tsx
+++ src/molecules/User.tsx
@@ -6,7 +6,7 @@ const User = () => {
   return (
     <div className='bg-red-50 font-poppins text-sm text-black flex flex-row items-center space-x-2 w-fit px-3 '>
         {/* <img src={logo} alt="logo" className="w-20 h-20 border-2 border-blue-600 rounded-full fit-contain" /> */}
-        <Image source={logo} alt="logo" />
+        <Image source={logo} alt="logo" radius="full" />
         <div className='sm:w-40 w-fit text-xl sm:text-sm'>
           <div className='font-bold'>Joyadeep Limbu</div>
           {/* <div className=''>2022/04/21 10:45:05</div> */}
